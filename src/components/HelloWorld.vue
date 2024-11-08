acee662efafe9283e146e4355b2f499bc345ff81
kevin <kylerlin0728@gmail.com>
2024-11-08 13:40:37 +0800

    暫存 fabric

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <canvas id="myCanvas"></canvas>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+<script setup>
+  // import { onMounted, nextTick } from "vue";
+  import * as fabric from "fabric";
+  const canvas = new fabric.Canvas(document.getElementById("myCanvas"));
+  // onMounted(() => {
+  //   nextTick(() => {
+  //     canvas = new fabric.Canvas("myCanvas", {
+  //       width: 500,
+  //       height: 500,
+  //       backgroundColor: "white",
+  //     });
+  //   });
+  // });
+  const demoImg = "https://fabricjs.github.io/assets/pug.jpg";
+  // const imgElement = new Image();
+  // imgElement.src = demoImg;
+  // const imgInstance = new fabric.Image(
+  //   "https://fabricjs.github.io/assets/pug.jpg",
+  //   {
+  //     left: 0,
+  //     top: 0,
+  //     width: 150,
+  //     height: 150,
+  //   }
+  // );
+  fabric.Image.fromURL(demoImg).then((img) => {
+    img.set({
+      dirty: true,
+      left: 0,
+      top: 0,
+      width: 150,
+      height: 150,
+    });
+    canvas.add(img);
+  });
+  // eslint-disable-next-line no-undef
+  // const imgInstance = new FabricImage(demoImg, {
+  //   left: 0,
+  //   top: 0,
+  //   width: 150,
+  //   height: 150,
+  // });
+  // canvas.add(imgInstance);
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
